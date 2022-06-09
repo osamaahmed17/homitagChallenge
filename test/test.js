aee039fa06337735f44287d5f1b7472fd12c039c
osamaahmed17 <osamaahmedtahir17@gmail.com>
2022-06-09 21:17:32 +0500

    Tests Added

---
+++ test/test.js
@@ -0,0 +1,188 @@
+var supertest = require("supertest");
+var should = require("should");
+let movieModel = require('../model/movieModel');
+
+
+
+var server = supertest.agent("http://localhost:3000");
+
+
+describe("Movies Test", function () {
+  it("It should GET movies", function (done) {
+    server
+      .get("/rest/api/v1/movie")
+      .expect("Content-type", /json/)
+      .expect(200)
+      .end(function (err, res) {
+        res.status.should.equal(200);
+        done();
+      });
+  });
+
+  it("It should CREATE movie", function (done) {
+    let movie = {
+      "name": "Superman",
+      "description": "Best super action movie!",
+      "releaseDate": "2021-01-15T06:31:15.000Z",
+      "genres": "Animation",
+      "duration": 20,
+      "rating": 5
+    }
+    server
+      .post('/rest/api/v1/movie')
+      .send(movie)
+      .expect("Content-type", /json/)
+      .expect(201)
+      .end(function (err, res) {
+        res.status.should.equal(201);
+        done();
+      });
+  });
+
+  it("It should DELETE movie by given ID", function (done) {
+    let movie = {
+      "name": "Superman",
+      "description": "Best super action movie!",
+      "releaseDate": "2021-01-15T06:31:15.000Z",
+      "genres": "Animation",
+      "duration": 20,
+      "rating": 5
+    }
+    let createdID
+    server
+      .post('/rest/api/v1/movie')
+      .send(movie)
+      .expect("Content-type", /json/)
+      .expect(201)
+      .end(function (err, res) {
+        createdID = '/rest/api/v1/movie/' + res._body.data.item.id
+        server
+          .delete(createdID)
+          .expect("Content-type", /json/)
+          .expect(201).end(function (err, res) {
+            done()
+
+          })
+      })
+
+  });
+
+  it('It should UPDATE movie by given ID', (done) => {
+    let movie = {
+      "name": "Superman",
+      "description": "Best super action movie!",
+      "releaseDate": "2021-01-15T06:31:15.000Z",
+      "genres": "Animation",
+      "duration": 20,
+      "rating": 5
+    }
+    let updateMovie = {
+      "name": "Superman",
+      "description": "Best super action movie!",
+      "releaseDate": "2021-01-15T06:31:15.000Z",
+      "genres": "Animation",
+      "duration": 20,
+      "rating": 5
+    }
+
+    let createdID
+    server
+      .post('/rest/api/v1/movie')
+      .send(movie)
+      .expect("Content-type", /json/)
+      .expect(201)
+      .end(function (err, res) {
+        createdID = '/rest/api/v1/movie/' + res._body.data.item.id
+        server
+          .put(createdID)
+          .send(updateMovie)
+          .expect("Content-type", /json/)
+          .expect(201).end(function (err, res) {
+            done()
+
+          })
+      })
+  })
+});
+
+describe("Genre Test", function () {
+  it("It should GET Genre", function (done) {
+    server
+      .get("/rest/api/v1/genre")
+      .expect("Content-type", /json/)
+      .expect(200)
+      .end(function (err, res) {
+        res.status.should.equal(200);
+        done();
+      });
+  });
+
+  it("It should CREATE genre", function (done) {
+    let genre = {
+      "name": "Animation",
+      "description": "English Cartoons"
+    }
+    server
+      .post('/rest/api/v1/genre')
+      .send(genre)
+      .expect("Content-type", /json/)
+      .expect(201)
+      .end(function (err, res) {
+        res.status.should.equal(201);
+        done();
+      });
+  });
+
+  it("It should DELETE movie by given ID", function (done) {
+    let genre = {
+      "name": "Animation",
+      "description": "English Cartoons"
+    }
+    let createdID
+    server
+      .post('/rest/api/v1/genre')
+      .send(genre)
+      .expect("Content-type", /json/)
+      .expect(201)
+      .end(function (err, res) {
+        createdID = '/rest/api/v1/genre/' + res._body.data.item.id
+        server
+          .delete(createdID)
+          .expect("Content-type", /json/)
+          .expect(201).end(function (err, res) {
+            done()
+
+          })
+      })
+
+  });
+
+  it('It should UPDATE movie by given ID', (done) => {
+    let genre = {
+      "name": "Animation",
+      "description": "English Cartoons"
+    }
+    let updatedGenre = {
+      "name": "Action",
+      "description": "Action Movies"
+    }
+
+    let createdID
+    server
+      .post('/rest/api/v1/movie')
+      .send(genre)
+      .expect("Content-type", /json/)
+      .expect(201)
+      .end(function (err, res) {
+        createdID = '/rest/api/v1/movie/' + res._body.data.item.id
+        server
+          .put(createdID)
+          .send(updatedGenre)
+          .expect("Content-type", /json/)
+          .expect(201).end(function (err, res) {
+            done()
+
+          })
+      })
+  })
+});
